c037121f9b7caf8b505d63d2046b7537c38cd22d
marius david <marius@mariusdavid.fr>
2025-02-09 17:40:08 +0100

    better spacing of books in book line

--- godot/LibraryEntity/book_line.gd
+++ godot/LibraryEntity/book_line.gd
@@ -16,12 +16,28 @@ func _ready() -> void:
 func _load_static():
 	var book_scene = preload("res://LibraryEntity/Book.tscn")
 	var books = $"/root/GlobalLibrary".get_book_range_from_distance(start_position, length, false)
+	
+	var previous_book = null
+	var free_space_total_length = 0
+	var free_space_count = 0
+	for book in books:
+		if previous_book != null:
+			free_space_total_length += book.get_distance() - previous_book.get_distance() - book.get_width()
+			free_space_count += 1
+		previous_book = book
+	
+	var average_free_space = 0
+	if free_space_count != 0:
+		average_free_space = free_space_total_length / free_space_count
+	
+	var current_distance = 0
 	for book in books:
 		var node = book_scene.instantiate()
 		node.book_info = book
 		node.rotate(Vector3.FORWARD, PI/2)
-		var distance_to_start = book.get_distance() - start_position
-		node.position = Vector3((0.001 * distance_to_start), 0.20, 0)
+		node.position = Vector3((0.001 * current_distance), 0.20, 0)
+		current_distance += book.get_width()
+		current_distance += average_free_space
 		add_child(node)
 	$PreviewMesh.set("visible", false)
 
